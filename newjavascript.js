e799ce6e2faa96eb0f989772b6b1897ace7477d3
Anh <khuongvietanh01@gmail.com>
2018-12-24 00:10:16 +0700

    update

--- newjavascript.js
+++ newjavascript.js
@@ -16,7 +16,11 @@ $(document).ready(function () {
         {
             count = countImg.length - 1;
         }
+        setPoiterEvents(1);
         animate(count, slides);
+        setTimeout(function () {
+            setPoiterEvents(0);
+        }, 1000);
     });
     //click button next
     $(document).on("click", ".next__js", function () {
@@ -28,17 +32,27 @@ $(document).ready(function () {
         {
             count = countImg.length - 1;
         }
+        setPoiterEvents(1);
         animate(count, slides);
+        setTimeout(function () {
+            setPoiterEvents(0);
+        }, 1000);
     });
     // click img Nav
     $(document).on("click", ".container__items_nav li", function () {
         var index = $(this).index();
         count = index;
+        setPoiterEvents(1);
         animate(count, slides);
+        setTimeout(function () {
+            setPoiterEvents(0);
+        }, 1000);
     });
+
+    animate(0, slides);
     //funtion animate
     function animate(count, slides) {
-        slides.animate({right: count * rightList}, 1000);
+        slides.animate({right: count * rightList}, 900);
         noneOpactity();
         $(".container__items_nav li").eq(count).animate({opacity: 0.5});
     }
@@ -46,5 +60,34 @@ $(document).ready(function () {
     function noneOpactity() {
         $(".container__items_nav li").animate({opacity: 1});
     }
+    //call funtion
+    autoPlay();
+    //funtion autoplay
+    function autoPlay() {
+        var autoPlay = setInterval(function () {
+            count++;
+            if (count >= countImg.length)
+            {
+                count = 0;
+            } else if (count < 0)
+            {
+                count = countImg.length - 1;
+            }
+            animate(count, slides);
+        }, 2000);
+    }
+    //When the animation is being processed, the blanket does not allow the new animation to be clicked
+    //When the animation is finished,allow the new animation to be clicked
+    function setPoiterEvents(type) {
+        var itemsNav = document.getElementsByClassName("container__items_nav__js");
+        if (type === 1) {
+            for (var i = 0; i < itemsNav.length; i++) {
+                itemsNav[i].setAttribute('style', 'pointer-events: none;');
+            }
+        } else {
+            for (var i = 0; i < itemsNav.length; i++) {
+                itemsNav[i].setAttribute('style', 'pointer-events: auto;');
+            }
+        }
+    }
 });
-
